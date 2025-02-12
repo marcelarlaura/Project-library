18363aa514439dcdd65e2959e557c888820bfc70
marcelarlaura <171378249+marcelarlaura@users.noreply.github.com>
2025-02-12 16:30:25 -0500

    add options to mark book as read and delete book

--- javascript.js
+++ javascript.js
@@ -9,6 +9,8 @@ const genre = document.querySelector('#genre');
 const release = document.querySelector('#release');
 const publisher = document.querySelector('#publisher');
 const myLibrary = [];
+let idvariable = 0;
+
 
 function Book(arg1, arg2,arg3, arg4, arg5, arg6){
     this.title = arg1;
@@ -17,6 +19,7 @@ function Book(arg1, arg2,arg3, arg4, arg5, arg6){
     this.genre = arg4;
     this.year = arg5;
     this.publisher = arg6;
+    this.id = '0' + idvariable;
 }
 
 function addBookToLibrary(arg){
@@ -36,7 +39,28 @@ function displayBooks(arg){
     let lastBookAuthor = arg[lastBookIndex].author;
     let div = document.createElement('div');
     div.textContent = `${lastBookTitle} ${lastBookAuthor}`;
+    div.setAttribute('id', arg[lastBookIndex].id);
     bookHolder.append(div);
+    let resetButton = document.createElement('button');
+    resetButton.textContent = 'delete';
+    resetButton.classList.add('delete-book');
+    resetButton.setAttribute('id', arg[lastBookIndex].id);
+    div.append(resetButton);
+    var read = document.createElement('input');
+    read.type = 'checkbox';
+    read.classList.add('read-status');
+    div.append(read);
+    div.append('read');
+    resetButton.addEventListener('click', ()=>{
+        div.remove();
+        for (let i=0; i<myLibrary.length; i++){
+            if (resetButton.id == myLibrary[i].id){
+                myLibrary.splice(i,1);
+                i--;
+            }
+        }
+        console.log(myLibrary);
+    })
 }
 
 newForm.addEventListener('click', () =>{
@@ -46,10 +70,16 @@ newForm.addEventListener('click', () =>{
 createBook.addEventListener('click', (event)=>{
     event.preventDefault();
     let bookSample = new Book(title.value,author.value,pages.value,genre.value,release.value,publisher.value);
-    console.log(bookSample);
     myLibrary.push(bookSample);
+    idvariable++;
     displayBooks(myLibrary);
+    title.value = '';
+    author.value = '';
+    pages.value = '';
+    release.value = '';
+    publisher.value = '';
 })
+
 
 
 
